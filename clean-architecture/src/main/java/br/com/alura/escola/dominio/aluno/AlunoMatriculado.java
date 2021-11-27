1a12b7892bf2311f5c8f61075f4f8cc2692668cb
Daniel <daniel.agostinho@hotmail.com>
2021-11-27 12:10:19 -0300

    Eventos de Domínio

---
+++ clean-architecture/src/main/java/br/com/alura/escola/dominio/aluno/AlunoMatriculado.java
@@ -0,0 +1,24 @@
+package br.com.alura.escola.dominio.aluno;
+
+import br.com.alura.escola.dominio.Evento;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+
+public class AlunoMatriculado implements Evento {
+
+    @Getter
+    private final CPF cpfDoAluno;
+    private final LocalDateTime momento;
+
+    public AlunoMatriculado(CPF cpfDoAluno) {
+        this.cpfDoAluno = cpfDoAluno;
+        this.momento = LocalDateTime.now();
+    }
+
+    @Override
+    public LocalDateTime momento() {
+        return momento;
+    }
+}
