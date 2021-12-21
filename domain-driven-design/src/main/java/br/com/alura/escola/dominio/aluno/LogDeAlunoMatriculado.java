e0e833f4b21c7631e7744847b9fc44d598ada0ea
Daniel <daniel.agostinho@hotmail.com>
2021-12-21 20:37:31 -0300

    Publicando eventos

--- domain-driven-design/src/main/java/br/com/alura/escola/dominio/aluno/LogDeAlunoMatriculado.java
+++ domain-driven-design/src/main/java/br/com/alura/escola/dominio/aluno/LogDeAlunoMatriculado.java
@@ -1,18 +1,25 @@
 package br.com.alura.escola.dominio.aluno;
 
-import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAluno;
+import br.com.alura.escola.dominio.evento.Evento;
+import br.com.alura.escola.dominio.evento.Ouvinte;
 
 import java.time.format.DateTimeFormatter;
 
-public class LogDeAlunoMatriculado {
+public class LogDeAlunoMatriculado extends Ouvinte {
 
-    public void reageAo(AlunoMatriculado evento) {
+    @Override
+    public void reageAo(Evento evento) {
         String momentoFormatado = evento.momento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
 
         System.out.println(String.format(
                 "Aluno com CPF %s matriculado em: %s",
-                evento.getCpfDoAluno().getNumero(),
+                ((AlunoMatriculado) evento).getCpfDoAluno().getNumero(),
                 momentoFormatado
         ));
+    }
+
+    @Override
+    protected boolean deveProcessar(Evento evento) {
+        return evento instanceof AlunoMatriculado;
     }
 }
