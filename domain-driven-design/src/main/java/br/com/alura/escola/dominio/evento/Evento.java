4150c390de55f44f7a8a4d81874cac371bd85dcd
Daniel <daniel.agostinho@hotmail.com>
2021-12-21 19:24:22 -0300

    Eventos de Domínio

---
+++ domain-driven-design/src/main/java/br/com/alura/escola/dominio/evento/Evento.java
@@ -0,0 +1,7 @@
+package br.com.alura.escola.dominio.evento;
+
+import java.time.LocalDateTime;
+
+public interface Evento {
+    LocalDateTime momento();
+}
--- domain-driven-design/src/test/java/br/com/alura/escola/dominio/aluno/AlunoTest.java
+++ domain-driven-design/src/test/java/br/com/alura/escola/dominio/aluno/AlunoTest.java
@@ -4,9 +4,32 @@ import br.com.alura.escola.dominio.template.AlunoTemplate;
 import br.com.alura.escola.dominio.template.TelefoneTemplate;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AlunoTest {
+
+    @Test
+    void deveriaPermiterAdicionarUmTelefone(){
+        Telefone telefoneUm = TelefoneTemplate.builder().build();
+
+        Aluno aluno = AlunoTemplate.builder().build();
+        aluno.adicionarTelefone(telefoneUm.getDdd(), telefoneUm.getNumero());
+
+        assertEquals(1, aluno.getTelefones().size());
+    }
+
+    @Test
+    void deveriaPermiterAdicionarDoisTelefones(){
+        Telefone telefoneUm = TelefoneTemplate.builder().build();
+        Telefone telefoneDois = TelefoneTemplate.builder().build();
+
+        Aluno aluno = AlunoTemplate.builder().build();
+        aluno.adicionarTelefone(telefoneUm.getDdd(), telefoneUm.getNumero());
+        aluno.adicionarTelefone(telefoneDois.getDdd(), telefoneDois.getNumero());
+
+        assertEquals(2, aluno.getTelefones().size());
+    }
 
     @Test
     void naoDeveriaPermitirAdicinarMaisDoqueDoisTelefones() {
