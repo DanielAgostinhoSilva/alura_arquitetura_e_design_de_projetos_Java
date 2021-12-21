9f29477f49bf3c0c9211ca35159e8412cabbd772
Daniel <daniel.agostinho@hotmail.com>
2021-12-21 19:31:37 -0300

    Reagindo a eventos

---
+++ domain-driven-design/src/main/java/br/com/alura/escola/dominio/aluno/LogDeAlunoMatriculado.java
@@ -0,0 +1,18 @@
+package br.com.alura.escola.dominio.aluno;
+
+import br.com.alura.escola.aplicacao.aluno.matricular.MatricularAluno;
+
+import java.time.format.DateTimeFormatter;
+
+public class LogDeAlunoMatriculado {
+
+    public void reageAo(AlunoMatriculado evento) {
+        String momentoFormatado = evento.momento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+
+        System.out.println(String.format(
+                "Aluno com CPF %s matriculado em: %s",
+                evento.getCpfDoAluno().getNumero(),
+                momentoFormatado
+        ));
+    }
+}
