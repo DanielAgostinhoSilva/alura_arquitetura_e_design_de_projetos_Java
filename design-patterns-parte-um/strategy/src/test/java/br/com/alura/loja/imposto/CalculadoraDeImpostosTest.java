c2d593bf7db3cd7c8aa2f34776adc08b3d339e66
Daniel <daniel.agostinho@hotmail.com>
2021-12-26 18:39:03 -0300

    01-03 Calculando impostos

---
+++ design-patterns-parte-um/strategy/src/test/java/br/com/alura/loja/imposto/CalculadoraDeImpostosTest.java
@@ -0,0 +1,31 @@
+package br.com.alura.loja.imposto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraDeImpostosTest {
+
+    @Test
+    public void deveCalcularImpostoICMS() {
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+
+        BigDecimal impostoCalculado = calculadora.calcular(orcamento, TipoImposto.ICMS);
+
+        assertEquals(new BigDecimal("10.00"), impostoCalculado);
+    }
+
+    @Test
+    public void deveCalcularImpostoISS() {
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+
+        BigDecimal impostoCalculado = calculadora.calcular(orcamento, TipoImposto.ISS);
+
+        assertEquals(new BigDecimal("6.00"), impostoCalculado);
+    }
+}
