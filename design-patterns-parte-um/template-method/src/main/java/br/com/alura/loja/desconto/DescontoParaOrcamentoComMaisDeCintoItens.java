33e329bea9f5e99646fe2c93dfdb1d6f26a5a628
Daniel <daniel.agostinho@hotmail.com>
2021-12-28 19:04:28 -0300

    PATTERNS PARTE 1 aula 03-02 Aplicando o pattern Template Method

---
+++ design-patterns-parte-um/template-method/src/main/java/br/com/alura/loja/desconto/DescontoParaOrcamentoComMaisDeCintoItens.java
@@ -0,0 +1,22 @@
+package br.com.alura.loja.desconto;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class DescontoParaOrcamentoComMaisDeCintoItens extends Desconto {
+
+    public DescontoParaOrcamentoComMaisDeCintoItens(Desconto proximo) {
+        super(proximo);
+    }
+
+    @Override
+    public BigDecimal efetuarCalculo(Orcamento orcamento) {
+        return orcamento.getValor().multiply(new BigDecimal("0.10"));
+    }
+
+    @Override
+    public boolean deveAplicar(Orcamento orcamento) {
+        return orcamento.getQuantidadeItens() > 5;
+    }
+}
