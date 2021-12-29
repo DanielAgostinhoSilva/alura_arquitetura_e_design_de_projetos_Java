1b43701c261a5a711906df0a437e7eec7591f861
Daniel <daniel.agostinho@hotmail.com>
2021-12-29 15:39:26 -0300

    PATTERNS PARTE 1 aula 06-04 Aplicando o pattern Observer

---
+++ design-patterns-parte-um/observer/src/main/java/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -0,0 +1,24 @@
+package br.com.alura.loja.pedido;
+
+import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class GeraPedidoHandler {
+
+    private List<AcaoAposGerarPedido> acoes;
+
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+        this.acoes = acoes;
+    }
+
+    public void executa(GeraPedido geraPedido) {
+        Orcamento orcamento = new Orcamento(geraPedido.getValorOrcamento(), geraPedido.getQuantidadeItens());
+
+        Pedido pedido = new Pedido(geraPedido.getCliente(), LocalDateTime.now(), orcamento);
+
+        acoes.forEach( acao -> acao.executarAcao(pedido));
+    }
+}
